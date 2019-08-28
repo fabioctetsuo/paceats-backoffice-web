909e6ea25924d5d0e2c3b351212bc0f437614e9b
Fabio Tetsuo <fabioctetsuo@gmail.com>
2019-08-28 04:41:04 +0000

    Merged in feature/implement-mock-api (pull request #1)
    Feature: Adding mock api configuration
    
    Approved-by: Fabio Tetsuo <fabioctetsuo@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <p>React here!</p>
+    </div>
+  );
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('app'));
