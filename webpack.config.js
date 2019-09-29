c17931663b218473fcd4b0d1be3fae1af7fa5653
Fabio Tetsuo <fabioctetsuo@gmail.com>
2019-09-29 20:39:49 -0300

    fiz: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
 const path = require('path');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: ["src/index.js"],
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
       },
       {
         test: /\.(sa|sc|c)ss$/,
@@ -35,24 +34,25 @@ module.exports = {
         use: [
           {
             loader: 'file-loader',
-          }
-        ]
+          },
+        ],
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
+      template: './public/index.html',
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
   devtool: 'cheap-module-eval-source-map', // this helps to browser to point to the exact file in the console, helps in debug
   devServer: {
     contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true // this prevents the default browser full page refresh on form submission and link change
-  }
+    historyApiFallback: true, // this prevents the default browser full page refresh on form
+    // submission and link change
+  },
 };
